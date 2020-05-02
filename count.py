720dd6c37302690a5d53cdb08422496176384ae2
QMinjia <maidoudou@sjtu.edu.cn>
2020-05-01 22:56:31 -0400

    Create count.py

---
+++ count.py
@@ -0,0 +1,112 @@
+import nltk
+import random
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
+import re
+import csv
+
+    
+trainDocuments = []
+trainLabels = []
+trainFood = []
+trainService = []
+trainAmbience = []
+
+with open('E:/github/SI-561-Project/train.csv', 'r', encoding='UTF-8') as f:
+    csvreader = csv.reader(f)
+    headers = next(f)
+    for line in csvreader:
+        trainDocuments.append(line[4])
+        trainLabels.append(line[0])
+        trainFood.append(line[1])
+        trainService.append(line[2])
+        trainAmbience.append(line[3])
+        # trainLabels.append(int(line[0])>4)
+        
+testDocuments = []
+testLabels = []
+testFood = []
+testService = []
+testAmbience = []
+c2 = 0
+with open('E:/github/SI-561-Project/test.csv', 'r', encoding='UTF-8') as f:
+    csvreader = csv.reader(f)
+    for line in csvreader:
+        testDocuments.append(line[4])
+        testLabels.append(line[0])
+        testFood.append(line[1])
+        testService.append(line[2])
+        testAmbience.append(line[3])
+        # testLabels.append(int(line[0])>4)
+        # if int(line[0])<4: 
+            # c2 += 1
+# print(c2)
+
+vectorizer = TfidfVectorizer(analyzer = "word", stop_words = 'english', preprocessor = None, encoding='utf-8', ngram_range=(1,2), sublinear_tf = True)
+documentsVectors = vectorizer.fit_transform(trainDocuments+testDocuments)
+size1 = len(trainDocuments)
+
+clf = svm.LinearSVC(C = 1.5)
+clf.fit(documentsVectors[0:size1],trainLabels[0:size1])
+prediction = clf.predict(documentsVectors[size1:])
+count = 0
+c = 0
+for ans, truth in zip(prediction, testLabels):
+    if ans == truth:
+        count += 1
+    # if c==1098:
+        # print('Review: ' + testDocuments[c])
+        # print('Predictions: ')
+        # print('    Overall: ' + '★'*int(ans) + '☆'*(5-int(ans)))
+    if ans != '5':
+        print(ans, c)
+    c+=1
+# print(count/len(testLabels))
+
+clf2 = svm.LinearSVC(C = 1.5)
+clf2.fit(documentsVectors[0:size1],trainFood[0:size1])
+prediction = clf2.predict(documentsVectors[size1:])
+count = 0
+c = 0
+for ans, truth in zip(prediction, testFood):
+    if ans == truth:
+        count += 1
+    # if c==1098:
+        # print('    Food: ' + '★'*int(ans) + '☆'*(5-int(ans)))
+    if ans != '5':
+        print(ans, c)
+    c+=1
+# print(count/len(testFood))
+
+clf3 = svm.LinearSVC(C = 1.5)
+clf3.fit(documentsVectors[0:size1],trainService[0:size1])
+prediction = clf3.predict(documentsVectors[size1:])
+count = 0
+c = 0
+for ans, truth in zip(prediction, testService):
+    if ans == truth:
+        count += 1
+    # if c==1098:
+        # print('    Service: ' + '★'*int(ans) + '☆'*(5-int(ans)))
+    if ans != '5':
+        print(ans, c)
+    c+=1
+# print(count/len(testService))
+
+clf4 = svm.LinearSVC(C = 1.5)
+clf4.fit(documentsVectors[0:size1],trainAmbience[0:size1])
+prediction = clf4.predict(documentsVectors[size1:])
+count = 0
+c = 0
+for ans, truth in zip(prediction, testAmbience):
+    if ans == truth:
+        count += 1
+    # if c==1098:
+        # print('    Ambience: ' + '★'*int(ans) + '☆'*(5-int(ans)))
+    if ans != '5':
+        print(ans, c)
+    c+=1
+# print(count/len(testAmbience))
